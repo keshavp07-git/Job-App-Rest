23ffdecc2f8098082e1d1545f0bf4fecf5b81cd4
keshavp07-git <keshav.prajapati830@gmail.com>
2024-10-08 17:49:30 +0530

    React UI connection by @crossOrigin

--- src/main/java/com/keshav/Job/App/Rest/JobRestController.java
+++ src/main/java/com/keshav/Job/App/Rest/JobRestController.java
@@ -4,14 +4,12 @@ import com.keshav.Job.App.Rest.model.JobPost;
 import com.keshav.Job.App.Rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 //@Controller
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //This helps to connect React UI with our API and solve problem of Cross Origin.
 public class JobRestController {
 
     @Autowired
