7e1352279f968c7193f2551b58f581d708a141f0
keshavp07-git <keshav.prajapati830@gmail.com>
2024-10-18 15:21:37 +0530

    AOP Joint Point

--- src/main/java/com/keshav/Job/App/Rest/aop/LoggingAspect.java
+++ src/main/java/com/keshav/Job/App/Rest/aop/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.keshav.Job.App.Rest.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -7,20 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-@Aspect // This annotation tells that this is Aspect , This class will hold concerns.
+@Aspect
 public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    // Declare type of execution before , after , after execution , around execution.
-    // return type class-name.method-name(args)
-    // all type of return type use * if not use proper name , int or string
-    // all type of class name use * if not use proper name , com.keshav.Job.App.Rest.service;
-    // all type of args use (..) if not use proper name , jobservice etc
-
-
-    // @Before("execution(* *.*(..)") // Eg. of Annotation Which work of everything
-    @Before("execution(* com.keshav.Job.App.Rest.service.JobService.*(..))") // This is the advice type, This work for only one class-name ---> JobService
-    public void logMethodCall(){
-        LOGGER.info("Method Called");
+   // @Before("execution(* com.keshav.Job.App.Rest.service.JobService.getPost(..))") // I determined when i want a log and that point is getPost is the concept of Joint Point
+    @Before("execution(* com.keshav.Job.App.Rest.service.JobService.getPost(..)) || execution(* com.keshav.Job.App.Rest.service.JobService.deleteJob(..))")
+    //Here we are using Joint Point for two method simultaneously.
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info(" Method Called " + jp.getSignature().getName());
     }
 }
