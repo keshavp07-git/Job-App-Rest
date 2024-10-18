0395989dce6484095134f180a2c3e82f69608f08
keshavp07-git <keshav.prajapati830@gmail.com>
2024-10-18 17:37:22 +0530

    Negative Input Validation

--- src/main/java/com/keshav/Job/App/Rest/aop/PerformanceMonitoringAspect.java
+++ src/main/java/com/keshav/Job/App/Rest/aop/PerformanceMonitoringAspect.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class PerformanceMonitoringAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceMonitoringAspect.class);
-    @Around("execution(* com.keshav.Job.App.Rest.service.JobService.getPost(..))")// Around is used to test performance
+    @Around("execution(* com.keshav.Job.App.Rest.service.JobService.*(..))")// Around is used to test performance
     // it is like something to monitor execution speed , it should be there at start and end time of execution
     public Object MonitorTime(ProceedingJoinPoint jp) throws Throwable { // To calculate time we need ProceedingJointPoint which will at start and end of execution
         long start = System.currentTimeMillis();
@@ -20,7 +20,7 @@ public class PerformanceMonitoringAspect {
         //Proceed return Object so we change MonitorTime method type void to Object, //otherwise data will not be shown but
         // in console we see the time but in Postman we want to see data we have tu use Object type method
         long end = System.currentTimeMillis();
-        LOGGER.info("Time Taken " + (end-start) + "ms");
+        LOGGER.info("Time Taken by " + ": " + jp.getSignature().getName()+ " :" +(end-start) + " ms");
         return obj;
 
     }
